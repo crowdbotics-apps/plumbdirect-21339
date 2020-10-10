f421b511fc5c19392f68d5756826c7d01d2e953a
Crowdbotics <team@crowdbotics.com>
2020-10-10 19:22:41 +0000

    Update models

--- backend/plumbdirect_21339/wsgi.py
+++ backend/plumbdirect_21339/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plumbdirect_21339.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plumbdirect_21339.settings")
 
 application = get_wsgi_application()
